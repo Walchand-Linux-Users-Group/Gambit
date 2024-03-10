99a85d193e7d0e6982306e2780717ea3aa5b8c88
Utkarsh Mandape <70505181+UtkarshM-hub@users.noreply.github.com>
2024-03-10 12:09:41 +0530

    Update features.go

--- Features/features.go
+++ Features/features.go
@@ -17,36 +17,27 @@ func NewClass() *Manager {
 	}
 }
 
+
+
+
+// ------------  CHANGE FOLLOWING CODE INORDER TO COMPLETE THE TASKS ------------------- //
+
 func (M Manager) Hello() string {
 	return "Hello From Manager"
 }
 
 func (M *Manager) Insert(cost int, status bool, assigned_to, project_name string) {
-
-	// if cost < 0 || assigned_to == "" || project_name == "" {
-	// 	fmt.Println("Please Provide Appropriate Values")
-	// 	return
-	// }
-
-	// temp := Project{
-	// 	Project_Name:      project_name,
-	// 	Assigned_to:       assigned_to,
-	// 	Completion_Status: status,
-	// 	Cost:              cost,
-	// }
-
-	// M.Projects = append(M.Projects, temp)
+	
 }
 
 func (M *Manager) Sum() {
-	// sum := 0
-	// for _, v := range M.Projects {
-	// 	sum += v.Cost
-	// }
-	// fmt.Println(sum)
+	
 }
 
 func (M *Manager) List() {
+
+	// CODE OF THE 'List' FUNCTION IS GIVEN BELOW, UNCOMMENT AND USE IT DIRECTLY :)
+	
 	// fmt.Println("----- List Of Projects ------")
 	// for i, v := range M.Projects {
 	// 	fmt.Printf("\nIndex: %v\nProject Name: %v\nAssigned To: %v\nCompletion Status: %v\nCost: %v\n", i, v.Project_Name, v.Assigned_to, v.Completion_Status, v.Cost)
@@ -54,29 +45,9 @@ func (M *Manager) List() {
 }
 
 func (M *Manager) Update(status bool, assigned_to string, project_name string, index, cost int) {
-	// if cost < 0 || assigned_to == "" || project_name == "" {
-	// 	fmt.Println("Please Provide Appropriate Values")
-	// 	return
-	// }
-	// for i := range M.Projects {
-	// 	if i == index {
-	// 		M.Projects[i] = Project{
-	// 			Project_Name:      project_name,
-	// 			Assigned_to:       assigned_to,
-	// 			Cost:              cost,
-	// 			Completion_Status: status,
-	// 		}
-	// 		break
-	// 	}
-	// }
+	
 }
 
 func (M *Manager) Delete(index int) {
-	// for i := range M.Projects {
-	// 	if i == index {
-	// 		M.Projects = append(M.Projects[:index], M.Projects[index+1:]...)
-	// 		return
-	// 	}
-	// }
-	// fmt.Println("Index out of bound")
+	
 }
